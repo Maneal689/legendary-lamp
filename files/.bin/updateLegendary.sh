105063de46643e00c4a5a2a13f37fd7a32918809
Maneal689 <g3douilly@gmail.com>
2019-05-02 01:07:58 +0200

    add oh my zsh folder

--- files/.bin/updateLegendary.sh
+++ files/.bin/updateLegendary.sh
@@ -15,6 +15,7 @@ sudo mkdir -p $FOLDER/config/emacs
 
 sudo cp -r ~/.bin $FOLDER
 sudo cp -r ~/.fonts $FOLDER
+sudo cp -r ~/.oh-my-zsh $FOLDER
 sudo cp ~/{.eslintrc.js,.prettierrc.js,.conkyrc,.tmux.conf,.zshrc} $FOLDER
 
 sudo cp -r ~/.config/{i3,polybar,rofi,termite,compton.conf} $FOLDER/config/
